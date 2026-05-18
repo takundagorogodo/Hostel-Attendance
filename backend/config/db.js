1641152fe095a822fb063aed1f2773f743e2584e
GOROGODO TAKUNDA LEONARD <gorogodotakundah26@gmail.com>
2026-05-18 08:20:13 +0530

    Add files via upload

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDb = async () =>{
+    try {
+        const conn =await mongoose.connect(process.env.MONGO_URL);
+
+        console.log(`MongoDb Connected : ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+
+        process.exit(1);
+    }
+};
+
+export default connectDb;
