4e3170b6ac2b09cda9e6cb4944271af4bfb71231
GOROGODO TAKUNDA LEONARD <gorogodotakundah26@gmail.com>
2026-05-20 04:29:47 +0530

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,43 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDb  from "./config/db";
-
+import connectDb from "./config/db.js";
+import mainRoutes from "./routes/mainRoutes.js";
 
 dotenv.config();
 
-connectDb();
-
 const app = express();
 
+// Middleware
 app.use(express.json());
-app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("api",mainRoutes);
 
+app.use(cors({
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+// Routes
+app.use("/api", mainRoutes);
+
+// Error handler
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    success: false,
+    message: err.message
+  });
+});
+
+// Start server
+const startServer = async () => {
+  await connectDb();
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
+};
+
+startServer();
 
 export default app;
