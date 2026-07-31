4c9f236b4d00eb290afe71895a5e1389331b2780
takundagorogodo <gorogodotakundah26@gmail.com>
2026-07-31 10:20:32 +0530

    response.js

---
+++ backend/utils/response.js
@@ -0,0 +1,14 @@
+export const sendSuccess = (res, data = {}, message = 'Success', statusCode = 200) => {
+  return res.status(statusCode).json({
+    success: true,
+    message,
+    ...data,
+  });
+};
+
+export const sendError = (res, message = 'Something went wrong', statusCode = 500) => {
+  return res.status(statusCode).json({
+    success: false,
+    message,
+  });
+};
