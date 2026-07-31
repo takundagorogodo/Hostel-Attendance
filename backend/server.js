6b5c01cc7f170a2149fef4791f980b08f09b8c76
takundagorogodo <gorogodotakundah26@gmail.com>
2026-07-31 10:17:24 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,19 @@
-import "dotenv/config";
-import app from "./app.js";
+import 'dotenv/config';
+import app from './app.js';
+import logger from './utils/logger.js';
 
 const PORT = process.env.PORT || 5000;
 
-process.on("uncaughtException", (err) => {
-    console.error("uncaughtException:", err);
-    process.exit(1);
+process.on('uncaughtException', (err) => {
+  logger.error('uncaughtException:', err);
+  process.exit(1);
 });
 
 const server = app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  logger.info(`Server running on http://localhost:${PORT}`);
 });
 
-process.on("unhandledRejection", (reason) => {
-    console.error("unhandledRejection:", reason);
-    server.close(() => process.exit(1));
+process.on('unhandledRejection', (reason) => {
+  logger.error('unhandledRejection:', reason);
+  server.close(() => process.exit(1));
 });
