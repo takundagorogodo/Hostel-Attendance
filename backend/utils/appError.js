7e95e826ad21870bbca93af87c5bdec11032cdf7
takundagorogodo <gorogodotakundah26@gmail.com>
2026-07-31 10:22:36 +0530

    appError.js

---
+++ backend/utils/appError.js
@@ -0,0 +1,8 @@
+export class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode || 500;
+    this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
