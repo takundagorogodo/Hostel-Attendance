233c4c8c93758196237cb780631d3fe2ce662caa
takundagorogodo <gorogodotakundah26@gmail.com>
2026-08-05 08:03:23 +0530

    add

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const setAuthToken = (token) => {
+  if (token) {
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers.common['Authorization'];
+  }
+};
+
+export default api;
